c1ad23984f7fcb2fc8903224338562ff0e7a7957
jessica estrella <jess.m.estrella@gmail.com>
2025-05-20 17:24:52 +0100

    Home page containers

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,7 +1,12 @@
 class CreaturesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :home]
+
 
   def show
     @creature = Creature.find(params[:id])
   end
-
+  
+  def home
+    @creatures = Creature.where(available: true)
+  end
 end
