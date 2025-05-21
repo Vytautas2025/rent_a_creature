074fdb9535485ade7f932ee081f8e34681184380
Lukasz Krzizek <lukaszkrzi97@gmail.com>
2025-05-21 12:05:23 +0100

    creature-ownership-validation done

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,11 +1,11 @@
 class CreaturesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:show, :home]
-  
+
   def home
     @creatures = Creature.where(available: true)
   end
-  
+
   def show
     @creature = Creature.find(params[:id])
   end
@@ -23,17 +23,21 @@ class CreaturesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def edit
     @creature = Creature.find(params[:id])
+    redirect_to root_path, alert: "You can only edit your own creatures" unless @creature.user == current_user
   end
 
   def update
     @creature = Creature.find(params[:id])
-    if @creature.update(creature_params)
-      redirect_to creature_path(@creature)
+
+    if @creature.user != current_user
+      redirect_to root_path, alert: "You can only edit your own creatures"
+    elsif @creature.update(creature_params)
+      redirect_to creature_path(@creature), notice: "Creature successfully updated"
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
