61acb9a2dee86664d233edeecffb3afe2d476495
Shwetha Marumada <shwe.nie@gmail.com>
2025-05-23 14:20:24 +0100

    Delete option given for the Owner of the creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -60,6 +60,12 @@ class CreaturesController < ApplicationController
     end
   end
 
+  def destroy
+    @creature = Creature.find(params[:id])
+    @creature.destroy
+    redirect_to my_creatures_path, status: :see_other
+  end
+
   def book
     authenticate_user!
 
